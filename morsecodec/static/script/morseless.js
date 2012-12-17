de2ce5f28ba56d1e8a05fd6e788ef7cb451270f1
pdc <pdc@alleged.org.uk>
2012-12-17 13:20:16 +0000

    Make Ajax code trigger on keypress and paste events

--- morsecodec/static/script/morseless.js
+++ morsecodec/static/script/morseless.js
@@ -1,12 +1,25 @@
 var Morseless = (function($) {
     var ajaxUrl;
     var out;
+    var timeoutID;
 
     function update() {
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+            timeoutID = null;
+        }
         var morse = $('#morse').val();
         $.getJSON(ajaxUrl, {morse: morse}, function (data, textStatus, jqXHR) {
             out.val(data.text);
         });
+    }
+    function scheduleUpdate() {
+        if (!timeoutID) {
+            timeoutID = setTimeout(function () {
+                timeoutID = null;
+                update();
+            }, 750);
+        }
     }
 
     return {
@@ -24,7 +37,8 @@ var Morseless = (function($) {
             out.appendTo(div);
 
             $('#morse').change(update);
-
+            $('#morse').keypress(scheduleUpdate);
+            $('#morse').bind('paste', scheduleUpdate);
         }
     }
 })(jQuery);
